f65eea438334dce2014102060a914a406c305321
khalid09dev <mdkhalidhossen@hotmail.com>
2024-05-20 16:06:25 +0600

    .....

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,9 +1,13 @@
-import { useEffect, useRef, useState } from 'react';
+import { useContext, useEffect, useRef, useState } from 'react';
 import { loadCaptchaEnginge, LoadCanvasTemplate, validateCaptcha } from 'react-simple-captcha';
+import { AuthContext } from '../../providers/AuthProvider';
+import {Link} from 'react-router-dom';
 
 const Login = () => {
     const capchaRef = useRef(null);
     const [disabled, setDisabled] = useState(true);
+
+    const {signIn} = useContext(AuthContext);
 
     useEffect(() => {
         loadCaptchaEnginge(6);
@@ -15,9 +19,15 @@ const Login = () => {
         const email = form.email.value;
         const password = form.password.value;
         console.log(email, password)
+
+        signIn(email, password)
+        .then((result) => {
+            const user = result.user;
+            console.log(user);
+        })
     }
 
-    const handleValidateCapcha = (e) => {
+    const handleValidateCapcha = () => {
         const user_capcha_value = capchaRef.current.value;
         console.log(user_capcha_value);
         if(validateCaptcha(user_capcha_value)) {
@@ -63,6 +73,7 @@ const Login = () => {
                         <input disabled={disabled} type="submit" value="Login" className="btn btn-primary"/>
                     </div>
                 </form>
+                <p><small>New Here? <Link to="/signup">Create an Account</Link></small></p>
                 </div>
             </div>
             </div>
